783fdfaed3436c700af6b6b207118ed53afd06fe
Nataliia Tsirul <114382984+ch13Nataliia@users.noreply.github.com>
2024-10-07 17:13:51 +0100

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <div className="container">
+      Home page
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, libero possimus quo neque, eum voluptatem harum excepturi reiciendis quos doloribus praesentium quisquam minus facere itaque, magnam laboriosam quod? Quod, praesentium.</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ LoginPage.jsx
@@ -0,0 +1,15 @@
+
+const LoginPage = () => {
+  return (
+    <div className="mb-4">
+    <h1 className="text-4xl font-bold text-center mb-4">Login here</h1>
+      <form  className="max-w-md mx-auto ">
+        <input type="email" name="" id="" placeholder="email"/>
+        <input type="password" name="" id="" placeholder="password"/>
+        <button className="primary">Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginPage
